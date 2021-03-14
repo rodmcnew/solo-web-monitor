828a0f4ee1ab1d90958379c3c1e55af0da1da8f9
Rodney Mcnew <rmcnew@Rodneys-MacBook-Pro-2.local>
2021-03-14 17:53:30 -0500

    auto push

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
-import {MonitorDashboardContainer} from './features/monitor/MonitorDashboardContainer';
+import { MonitorDashboardContainer } from './features/monitor/MonitorDashboardContainer';
 function App() {
   return (
     <div className="App">
       <header>
         <span className="logoText">Solo Web Monitor</span>
-        <a style={{float: 'right'}} className="center" href="https://github.com/rodmcnew/solo-web-monitor">Documents</a>
+        <span style={{ float: 'right' }}>
+          <a className="center" href="/demo/reset-data">Reset Demo Data</a>
+          &nbsp;|&nbsp;
+          <a className="center" href="https://github.com/rodmcnew/solo-web-monitor">Documents</a>
+        </span>
       </header>
       <MonitorDashboardContainer />
     </div>
