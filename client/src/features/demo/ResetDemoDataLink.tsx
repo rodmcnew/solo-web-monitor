ab774daf2f042e0f652b1fa3f30291e244086b33
Rodney Mcnew <rmcnew@Rodneys-MacBook-Pro-2.local>
2021-04-07 20:14:35 -0500

    auto push

--- client/src/features/demo/ResetDemoDataLink.tsx
+++ client/src/features/demo/ResetDemoDataLink.tsx
@@ -3,7 +3,7 @@ import { demoApi } from './demoApi';
 
 export function ResetDemoDataLink() {
     const handleResetClick = useCallback(() => {
-        if (window.confirm('Reset all the data in the database to the initial demo values?')) {
+        if (window.confirm('Reset all the data in the database to the initial demo values?\n\nThis is safe to do since you are in the demo.')) {
             demoApi.resetDatabase().then(() => {
                 window.location.reload();
             })
