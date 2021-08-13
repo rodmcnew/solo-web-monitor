0ee4317461fc28a6a9a214aeca720dc01370d34a
Rod Mcnew <rodmcnew@gmail.com>
2021-08-13 01:02:26 -0500

    improve docs

--- client/src/features/demo/demoApi.ts
+++ client/src/features/demo/demoApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { HTTP_API_BASE_URL } from "../../config";
 
-export const demoApi = {
+export const demoApi = { //@TODO get rid of axios? do this with rtkq?
     resetDatabase: async (): Promise<void> => {
         return await axios.post(HTTP_API_BASE_URL + '/api/demo/reset-database');
     },
